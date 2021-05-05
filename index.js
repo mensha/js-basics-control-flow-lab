66d357be32f76e1de164324af78bc040f61a50be
mensha <msksalston@gmail.com>
2021-05-05 16:50:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
+function scuberGreetingForFeet(distance){
+
+  if (distance <= 400)  { 
+    return 'This one is on me!'
+  }
+
+  else if (distance > 1999 && distance < 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else { 
+    
+    return 'No can do.'
+  }
+  }
+
+
+function ternaryCheckCity(destination){
   // Write your code here!
+   destination === "NYC" ? 'Ok, sounds good.' : 'No go.'
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch()
 }
